0e422965558502a05207e183967ede4b4abc3a0d
Maria Zamyatina <maria.zamyatina.int@gmail.com>
2020-09-27 19:43:34 +0100

    add calc_seasonal_mean_in_layer()

--- proc_func.py
+++ proc_func.py
@@ -1,5 +1,9 @@
 from commons import GASES
 
+import iris
+
+from aeolus.coord import add_cyclic_point_to_cube
+
 
 def mmr_to_vmr(molar_mass, air_mass=28.97):
     """Convert mass mixing ratio to volume mixing ratio."""
@@ -12,3 +16,13 @@ for k, v in GASES.items():
         GASES[k]["mmr_to_vmr"] = mmr_to_vmr(GASES[k]["molar_mass"])
     except KeyError:
         pass
+
+
+def calc_seasonal_mean_in_layer(cube, season, layer):
+    """Calculate seasonal mean in a specified layer and add a cyclic longitude."""
+    ssn = iris.Constraint(season=season)
+    cb = cube.extract(ssn & layer).collapsed(
+        ["season", "level_height"], iris.analysis.MEAN
+    )
+    cyclic_cb = add_cyclic_point_to_cube(cb)
+    return cyclic_cb

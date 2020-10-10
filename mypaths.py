8501d2d8e3eb1510a9411d09acf1366e39464188
Maria Zamyatina <maria.zamyatina.int@gmail.com>
2020-10-10 23:32:10 +0100

    change path names

--- mypaths.py
+++ mypaths.py
@@ -1,4 +1,5 @@
 from pathlib import Path
 
+path_to_raw = Path.home() / "uea" / "phd" / "models" / "ukca" / "raw"
+path_to_processed = Path.home() / "uea" / "phd" / "models" / "ukca" / "processed"
 path_to_results = Path.home() / "uea" / "phd" / "results"
-path_to_ukca = Path.home() / "uea" / "phd" / "models" / "ukca" / "raw"

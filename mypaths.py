16b3bebf8b32ea46f3525d873600f274634b2b5c
Maria Zamyatina <maria.zamyatina.int@gmail.com>
2020-12-21 23:58:36 +0000

    clean up

--- mypaths.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-path_to_raw = Path.home() / "uea" / "phd" / "models" / "ukca" / "raw"
-path_to_processed = Path.home() / "uea" / "phd" / "models" / "ukca" / "processed"
-path_to_results = Path.home() / "uea" / "phd" / "results"

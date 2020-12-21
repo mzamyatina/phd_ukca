e4b5b62ce977ed237b62987913988751c2ea47a0
Maria Zamyatina <maria.zamyatina.int@gmail.com>
2020-12-21 23:59:28 +0000

    rename util files

---
+++ util_commons.py
@@ -0,0 +1,107 @@
+import iris
+
+EXPERIMENTS = {
+    "BASE OLD": "xojnd", # MeONO2 initialised with a non-zero steady state value from Banerjee2014 “Base”
+    "BASE": "xojng", # MeONO2 initialised with zero
+    "CHEM": "xojnh", # MeONO2 initialised with zero
+    "MARI": "xojni", # MeONO2 initialised with zero
+    "FIRE OLD1": "xojnc", # MeONO2 initialised with a non-zero steady state value from Banerjee2014 “Base”; iPrONO2 is chemically active, but no iPrONO2 ouput
+    "FIRE OLD2": "xojnv", # time 139 MeONO2 initialised with zero; unexplained jump in burdens
+    "FIRE OLD3": "xojnw", # time 193 # MeONO2 initialised with zero; unexplained jump in burdens; longer than xojnv
+    "FIRE": "xojnvw", # Stitch of xojnv and xojnw
+    "FULL": "xojnl", # MeONO2 initialised with zero
+#     'ORIG'    :'xolnb', # original CheST
+#     'UPDT'    :'xolna', # updated CheST
+}
+
+# Experiments history
+# CHEM xojne had a strange O3 burden
+# MARI xojnb MeONO2 wasn't initialized with zero and no n/iPrONO2 oceanic emissions
+# FIRE xojnc MeONO2 wasn't initialized with zero and wrong C2-C3 RONO2 outputs
+# FULL xojnk had a wrong setup
+
+STASH = {
+    "main": {
+        "temp": "air_temperature",
+        "air_mass": "m01s34i363",
+        "trop_mask": "m01s34i362",
+        "ch4": "mass_fraction_of_methane_in_air",
+        "c2h6": "mass_fraction_of_ethane_in_air",
+        "c3h8": "mass_fraction_of_propane_in_air",  # m01s34i018
+        "no": "mass_fraction_of_nitrogen_monoxide_in_air",
+        "no2": "m01s34i152",
+        "meono2": "mass_fraction_of_methyl_nitrate_in_air",
+        "etono2": "m01s34i096",  # was output using mass_fraction_of_methyl_ethyl_ketone_in_air stash code
+        "nprono2": "m01s34i097",  # was output using mass_fraction_of_toluene_in_air stash code
+        "iprono2": "m01s34i098",
+        "hono": "mass_fraction_of_nitrous_acid_in_air",
+        "ho2no2": "mass_fraction_of_peroxynitric_acid_in_air",
+        "hno3": "mass_fraction_of_nitric_acid_in_air",
+        "n2o5": "mass_fraction_of_dinitrogen_pentoxide_in_air",
+        "pan": "mass_fraction_of_peroxyacetyl_nitrate_in_air",
+        "ppan": "mass_fraction_of_peroxypropionyl_nitrate_in_air",
+        "o3": "mass_fraction_of_ozone_in_air",
+        "oh": "mass_fraction_of_hydroxyl_radical_in_air",
+    },
+    "extra": {
+        "trop_alt": "tropopause_altitude",
+        "co": "mass_fraction_of_carbon_monoxide_in_air",
+        "ho2": "mass_fraction_of_hydroperoxyl_radical_in_air",
+        "meoo": "mass_fraction_of_methyl_peroxy_radical_in_air",  # m01s34i083
+        "etoo": "mass_fraction_of_ethyl_peroxy_radical_in_air",  # m01s34i084
+        "nproo": "mass_fraction_of_n-propylperoxy_radical_in_air",  # m01s34i086
+        "iproo": "mass_fraction_of_isopropylperoxy_radical_in_air",  # m01s34i087
+        "hcho": "mass_fraction_of_formaldehyde_in_air",
+        "mecho": "mass_fraction_of_acetaldehyde_in_air",
+        "etcho": "mass_fraction_of_propanal_in_air",
+        "me2co": "mass_fraction_of_acetone_in_air",
+    },
+}
+
+GASES = {
+#     "main": {
+        "ch4": {"tex": "$CH_4$", "molar_mass": 16.0425,},
+        "c2h6": {"tex": "$C_2H_6$", "molar_mass": 30.0690,},
+        "c3h8": {"tex": "$C_3H_8$", "molar_mass": 44.0956,},
+        "no": {"tex": "NO", "molar_mass": 30.006, "nox": True, "noy": True},
+        "no2": {"tex": "$NO_2$", "molar_mass": 46.006, "nox": True, "noy": True},
+        "nox": {"tex": "$NO_x$",},
+        "meono2": {"tex": "$MeONO_2$", "molar_mass": 77.0394, "nox": False, "noy": True},
+        "etono2": {"tex": "$EtONO_2$", "molar_mass": 91.066, "nox": False, "noy": True},
+        "nprono2": {"tex": "$nPrONO_2$", "molar_mass": 105.0926, "nox": False, "noy": True},
+        "iprono2": {"tex": "$iPrONO_2$", "molar_mass": 105.0926, "nox": False, "noy": True},
+        "hono": {"tex": "HONO", "molar_mass": 47.013, "nox": False, "noy": True},
+        "ho2no2": {"tex": "$HO_2NO_2$", "molar_mass": 79.01224, "nox": False, "noy": True},
+        "hno3": {"tex": "$HNO_3$", "molar_mass": 63.012, "nox": False, "noy": True},
+        "n2o5": {"tex": "N_2O_5", "molar_mass": 108.01, "nox": False, "noy": True},
+        "pan": {"tex": "PAN", "molar_mass": 121.0489, "nox": False, "noy": True},
+        "ppan": {"tex": "PPN", "molar_mass": 135.0755, "nox": False, "noy": True},
+        "noy": {"tex": "$NO_y$",},
+        "o3": {"tex": "$O_3$", "molar_mass": 47.997,},
+        "oh": {"tex": "OH", "molar_mass": 17.007,},
+        "air": {"molar_mass": 28.9644},
+#     },
+#     "extra": {
+        "co": {"tex": "CO", "molar_mass": 28.010,},
+        "ho2": {"tex": "$HO_2$", "molar_mass": 33.006,},
+        "meoo": {"tex": "MeOO", "molar_mass": 47.0333,},
+        "etoo": {"tex": "EtOO", "molar_mass": 61.0599,},
+        "nproo": {"tex": "nPrOO", "molar_mass": 75.0865,},
+        "iproo": {"tex": "iPrOO", "molar_mass": 75.0865,},
+        "hcho": {"tex": "HCHO", "molar_mass": 30.0260,},
+        "mecho": {"tex": "MeCHO", "molar_mass": 44.0526,},
+        "etcho": {"tex": "EtCHO", "molar_mass": 58.0791,},
+        "me2co": {"tex": "Me2CO", "molar_mass": 58.0791,},
+#     },
+}
+
+LAYERS = {
+    "BL": {"iris_constr": iris.Constraint(level_height=lambda cell: 0 <= cell <= 2000), "tex": "Boundary layer (0-2 km)"},
+    "FT": {"iris_constr": iris.Constraint(level_height=lambda cell: 5000 <= cell <= 10000), "tex": "Free troposphere (5-10 km)"},
+}
+
+SEASONS = ["djf", "mam", "jja", "son"]
+
+# Coefficients to convert mass mixing ratio to volume mixing ratio
+# M_air = 28.97 # molar mass of dry air [g mol-1]
+# coeff_o3 = (M_air/47.997)*1e9
---
+++ util_plot_func.py
@@ -0,0 +1,23 @@
+import matplotlib.colors as colors
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+
+
+class MidpointNormalize(colors.Normalize):
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
+
+
+def use_paper_style():
+    """Load custom matplotlib style sheet."""
+    plt.style.use("util_paper.mplstyle")
+
+
+def use_draft_style():
+    """Load custom matplotlib style sheet."""
+    plt.style.use("util_draft.mplstyle")

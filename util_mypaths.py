e35672e3ac6f8f71c916febb10fe56b7ed4cc656
Maria Zamyatina <maria.zamyatina.int@gmail.com>
2021-03-21 23:16:47 +0000

    add path_to_atom

--- util_mypaths.py
+++ util_mypaths.py
@@ -3,3 +3,5 @@ from pathlib import Path
 path_to_raw = Path.home() / "uea" / "phd" / "models" / "ukca" / "raw"
 path_to_processed = Path.home() / "uea" / "phd" / "models" / "ukca" / "processed"
 path_to_results = Path.home() / "uea" / "phd" / "results"
+
+path_to_atom = Path.home() / "uea" / "phd" / "obs" / "ATom" / "nc" / "data"
